58ce689280a360092401af545b11d1302315ab83
Vinh Lam <vinh.lam@orientsoftware.com>
2021-03-19 14:37:54 +0700

    merge Khuong's code

---
+++ src/api/src/post.js
@@ -0,0 +1,7 @@
+const {
+  ElasticsearchServiceClient,
+} = require("@aws-sdk/client-elasticsearch-service");
+
+const client = new ElasticsearchServiceClient();
+
+module.exports.handler = async () => {};
