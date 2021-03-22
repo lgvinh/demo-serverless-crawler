3d9eb2aab84e70d9c9173cb0b2d6f3959dae3974
Khuong Vo <khuong.vo@zoi.de>
2021-03-22 17:27:02 +0700

    update code

--- src/api/src/controllers/list.js
+++ src/api/src/controllers/list.js
@@ -1,9 +1,43 @@
-module.exports.handler = async () => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Get products success!"
+const ElasticSearchRepository = require("../data-access-layer/repository");
+
+const client = new ElasticSearchRepository();
+
+module.exports.handler = async (event) => {
+  try {
+    const requestBody = {
+      from: 0,
+      size: 10
+    };
+
+    if (event.queryStringParameters && event.queryStringParameters.query) {
+      requestBody.query = {
+        bool: {
+          should: [
+            { match: { name: event.queryStringParameters.query } },
+            { match: { description: event.queryStringParameters.query } }
+          ]
+        }
+      }
+    }
+
+    const { hits: {
+        total: { value: total },
+        hits: data
+      }
+    } = await client.search({
+      index: "products",
+      body: requestBody
     })
-  };
-  return response;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: data.map(product =>  product._source ),
+        total
+      })
+    };
+    
+  } catch (error) {
+    console.log('Error: ', error);
+  }
 };
